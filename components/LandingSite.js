0e9dbc4598ce2fba6539ffe3db3e896a0c6434e1
KongkaMozumderSUST <kongkamozumder7@gmail.com>
2022-09-27 23:25:54 +0600

    updated ui

--- components/LandingSite.js
+++ components/LandingSite.js
@@ -1,22 +1,35 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
 import { rotationQuaternionForCoordinates } from "../utils/GetLocation";
 import * as THREE from "three";
 
 const MoonRadius = 2;
-const positionSatelite = [0, MoonRadius, 0];
+const positionSatelite = [0, MoonRadius + 0.04, 0];
 export default function LandingSite(props) {
   const rotationRef = useRef();
   const rotationQuaternion = rotationQuaternionForCoordinates(
-    props.site.lat,
-    props.site.long
+    props.station.lat,
+    props.station.long
   );
   console.log(rotationQuaternion);
   return (
     <group ref={rotationRef} quaternion={rotationQuaternion}>
       <mesh {...props} position={positionSatelite}>
-        <boxGeometry args={[0.05, 0.05, 0.05]} />
-        <meshStandardMaterial opacity={1} color={props.color} />
+        <sphereGeometry args={[0.025]} />
+        <meshBasicMaterial opacity={1} color={props.color} />
+        <Html>
+          <div
+            style={{
+              width: "fit-content",
+              height: "fit-content",
+              borderRadius: "2px",
+              backgroundColor: "white",
+              position: "absolute",
+            }}>
+            <pre>{props.station.apollo}</pre>
+          </div>
+        </Html>
       </mesh>
     </group>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { OrbitControls, Html } from "@react-three/drei";
 import LandingSite from "../components/LandingSite";
 import ShallowMoonquake from "../data/nakamura_1979_sm_locations.json";
 import DeepMoonquake from "../data/nakamura_2005_dm_locations.json";
+import ApolloStation from "../data/LM.json";
 import * as THREE from "three";
 
 //import "./App.css";
@@ -11,30 +12,72 @@ import * as THREE from "three";
 const MoonRadius = 2;
 
 const Sphere = () => {
-  const base = new THREE.TextureLoader().load("./moon.jpg");
   const ref = useRef();
-   //useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+  //useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+  const displacementMap = new THREE.TextureLoader().load(
+    "./displacementMap.jpg"
+  );
+  const base = new THREE.TextureLoader().load("./moon.jpg");
+
   return (
-    <mesh visible castShadow >
-      <directionalLight intensity={0.5} />
+    <mesh visible castShadow ref={ref}>
       <sphereGeometry attach='geometry' args={[MoonRadius, 100, 100]} />
-      <meshBasicMaterial map={base} color='white' />
+      <meshPhongMaterial
+        map={base}
+        displacementMap={displacementMap}
+        displacementScale={0.08}
+        shininess={30}
+        specular={0x111111}
+        emissive={0x111111}
+        color='white'
+      />
     </mesh>
   );
 };
+
+const SkyBox = () => {
+  const { scene } = useThree();
+  const loader = new THREE.CubeTextureLoader();
+  // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
+  const texture = loader.load([
+    "/skyboxArt/right.png",
+    "/skyboxArt/left.png",
+    "/skyboxArt/top.png",
+    "/skyboxArt/bottom.png",
+    "/skyboxArt/front.png",
+    "/skyboxArt/back.png",
+  ]);
+
+  // Set the scene background property to the resulting texture.
+  scene.background = texture;
+  return null;
+};
 export default function Home(props) {
   return (
-    <Canvas style={{ backgroundColor: "black", height: "100vh" }}>
-      <OrbitControls />
-      <Sphere />
+    <>
+      <Canvas style={{ backgroundColor: "black", height: "100vh" }}>
+        <OrbitControls />
+        <Sphere />
+        {ApolloStation.map((x) => (
+          <>
+            <LandingSite
+              station={{ lat: x.Lat, long: x.Long, apollo: x.LM }}
+              color='blue'
+            />
+          </>
+        ))}
+        {/*       
       {ShallowMoonquake.map((x) => (
-        <LandingSite site={{ lat: x.Lat, long: x.Long }} color='green' />
+        <LandingSite site={{ lat: x.Lat, long: x.Long }} color='blue' />
       ))}
       {DeepMoonquake.map((x) => (
         <LandingSite site={{ lat: x.Lat, long: x.Long }} color='red' />
-      ))}
-      <ambientLight />
-    </Canvas>
+      ))} */}
+        <SkyBox />
+        <ambientLight />
+        <directionalLight position={[5, 3, 5]} color={0xffffff} intensity={1} />
+      </Canvas>
+    </>
   );
 }
 
