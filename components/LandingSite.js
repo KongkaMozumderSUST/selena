4e0b638d2f20082d7d77276ae9d5ad88ce7a6998
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-09-28 21:41:32 +0600

    added changes

--- components/LandingSite.js
+++ components/LandingSite.js
@@ -5,16 +5,19 @@ import { rotationQuaternionForCoordinates } from "../utils/GetLocation";
 import { Popconfirm,Popover } from 'antd'
 import * as THREE from "three";
 import { useState } from "react";
+import 'antd/dist/antd.css'
 
 const MoonRadius = 2;
 const positionSatelite = [0, MoonRadius + 0.04, 0];
-export default function LandingSite(props) {
 
-  const mesh = useRef();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Rotate mesh every frame, this is outside of React without overhead
+const store = [
+  { name: 'outside', color: 'lightpink' },
+  { name: 'inside', color: 'lightblue' }
+  // ...
+]
+
+export default function LandingSite(props) {
+  var string= "station: Apollo"+props.station.apollo+ "\n\t"+", latitude: "+props.station.lat+ "\n\t"+", longitude: "+props.station.lat
   
   const rotationRef = useRef();
   const rotationQuaternion = rotationQuaternionForCoordinates(
@@ -24,24 +27,26 @@ export default function LandingSite(props) {
   console.log(rotationQuaternion);
   return (
     <group ref={rotationRef} quaternion={rotationQuaternion}>
-      <mesh {...props} position={positionSatelite}  scale={active ? 2 : 1} onClick={(e) => setActive(!active || console.log("Box # 1 was clicked"))}
-      // when <Hex/> is clicked I want to identify with a popup of conole each individual compnent clockwise
-      // onClick={(e) => { if  (window.confirm('You Selected box # 1')) this.setActive(!active) } }
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
+      <mesh {...props} position={positionSatelite}>
         <sphereGeometry args={[0.025]} />
         <meshBasicMaterial opacity={1} color={props.color} />
-        <Html distanceFactor={1.5}  >
+        <Html distanceFactor={1.5} >
+        <Popover title={props.station.apollo} content={string} onConfirm={props.onClick}  
+     okText="none" >
           <div
             style={{
               width: "fit-content",
               height: "fit-content",
               borderRadius: "2px",
+              marginTop:"5px",
               // backgroundColor: "white",
-              position: "absolute",
+              //position: "absolute",
             }}>
             <pre>{props.station.apollo}</pre>
           </div>
+            {/* <a color="white" href="#">.</a>  
+             <p style={{color:"blue"}}>.</p> */}
+           </Popover>
         </Html>
       </mesh>
     </group>
